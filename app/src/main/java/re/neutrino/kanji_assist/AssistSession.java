7e083114032eaf9021b9e65e6fc03893eeaf6b71
Aleksander Mistewicz <a.mistewicz@gmail.com>
2016-12-05 23:09:21 +0100

    Experiment with popup

--- app/src/main/java/re/neutrino/kanji_assist/AssistSession.java
+++ app/src/main/java/re/neutrino/kanji_assist/AssistSession.java
@@ -1,11 +1,15 @@
 package re.neutrino.kanji_assist;
 
+import android.app.Dialog;
 import android.app.assist.AssistContent;
 import android.app.assist.AssistStructure;
 import android.content.Context;
 import android.os.Bundle;
 import android.service.voice.VoiceInteractionSession;
 import android.util.Log;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AssistSession extends VoiceInteractionSession {
     public AssistSession(Context context) {
@@ -15,8 +19,14 @@ public class AssistSession extends VoiceInteractionSession {
     @Override
     public void onHandleAssist(Bundle data, AssistStructure structure, AssistContent content) {
         super.onHandleAssist(data, structure, content);
-
+        Toast.makeText(getContext(), "Begin scan", Toast.LENGTH_SHORT).show();
         Log.d(getContext().getPackageName(),
                 ((Integer)structure.getWindowNodeCount()).toString());
+        Dialog dialog = getWindow();
+        Log.d(getContext().getPackageName(), String.valueOf(dialog.isShowing()));
+        dialog.setContentView(R.layout.popup);
+        TextView textView = (TextView) dialog.findViewById(R.id.textView);
+        textView.setText("Example popup");
+        Toast.makeText(getContext(), "End scan", Toast.LENGTH_SHORT).show();
     }
 }
