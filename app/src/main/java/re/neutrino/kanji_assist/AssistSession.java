60ed3429d9b911c385f29d29b54e74b73aad31c4
Aleksander Mistewicz <a.mistewicz@gmail.com>
2016-12-05 23:09:33 +0100

    Experment with window

--- app/src/main/java/re/neutrino/kanji_assist/AssistSession.java
+++ app/src/main/java/re/neutrino/kanji_assist/AssistSession.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.service.voice.VoiceInteractionSession;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,10 +25,21 @@ public class AssistSession extends VoiceInteractionSession {
         Log.d(getContext().getPackageName(),
                 ((Integer)structure.getWindowNodeCount()).toString());
         Dialog dialog = getWindow();
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+        Log.d("set_pos (pre), x", String.valueOf(layoutParams.x));
+        Log.d("set_pos (pre), y", String.valueOf(layoutParams.y));
+        layoutParams.gravity = Gravity.NO_GRAVITY;
+        layoutParams.x = 100;
+        layoutParams.y = 100;
+        window.setAttributes(layoutParams);
         Log.d(getContext().getPackageName(), String.valueOf(dialog.isShowing()));
         dialog.setContentView(R.layout.popup);
         TextView textView = (TextView) dialog.findViewById(R.id.textView);
         textView.setText("Example popup");
+        layoutParams = window.getAttributes();
+        Log.d("set_pos (post), x", String.valueOf(layoutParams.x));
+        Log.d("set_pos (post), y", String.valueOf(layoutParams.y));
         Toast.makeText(getContext(), "End scan", Toast.LENGTH_SHORT).show();
     }
 }
