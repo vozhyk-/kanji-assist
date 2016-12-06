ad8ea1f93cc1eda0165118380578b749d82db775
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2016-12-06 12:08:56 +0100

    Find selected text and show it in the popup

--- app/src/main/java/re/neutrino/kanji_assist/AssistSession.java
+++ app/src/main/java/re/neutrino/kanji_assist/AssistSession.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.service.voice.VoiceInteractionSession;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -21,9 +22,15 @@ public class AssistSession extends VoiceInteractionSession {
     @Override
     public void onHandleAssist(Bundle data, AssistStructure structure, AssistContent content) {
         super.onHandleAssist(data, structure, content);
-        Toast.makeText(getContext(), "Begin scan", Toast.LENGTH_SHORT).show();
-        Log.d(getContext().getPackageName(),
-                ((Integer)structure.getWindowNodeCount()).toString());
+
+        String selected = getSelectedText(structure);
+        if (selected == null)
+            selected = "[No text selected]";
+
+        showPopup(selected);
+    }
+
+    private void showPopup(String text) {
         Dialog dialog = getWindow();
         Window window = dialog.getWindow();
         WindowManager.LayoutParams layoutParams = window.getAttributes();
@@ -36,10 +43,51 @@ public class AssistSession extends VoiceInteractionSession {
         Log.d(getContext().getPackageName(), String.valueOf(dialog.isShowing()));
         dialog.setContentView(R.layout.popup);
         TextView textView = (TextView) dialog.findViewById(R.id.textView);
-        textView.setText("Example popup");
+        textView.setText(text);
         layoutParams = window.getAttributes();
         Log.d("set_pos (post), x", String.valueOf(layoutParams.x));
         Log.d("set_pos (post), y", String.valueOf(layoutParams.y));
-        Toast.makeText(getContext(), "End scan", Toast.LENGTH_SHORT).show();
+    }
+
+    private String getSelectedText(AssistStructure structure) {
+        Toast.makeText(getContext(), "Begin scan", Toast.LENGTH_SHORT).show();
+
+        for (int i = 0; i < structure.getWindowNodeCount(); i++) {
+            final AssistStructure.WindowNode win =
+                    structure.getWindowNodeAt(i);
+            final String selected = walkViews(win.getRootViewNode(), 0);
+            if (selected != null) {
+                Toast.makeText(getContext(),
+                        "Found selected text", Toast.LENGTH_SHORT)
+                        .show();
+                return selected;
+            }
+        }
+
+        Toast.makeText(getContext(),
+                "Selected text not found", Toast.LENGTH_SHORT)
+                .show();
+        return null;
+    }
+
+    private String walkViews(AssistStructure.ViewNode node, int depth) {
+        if (node.getVisibility() != View.VISIBLE)
+            return null;
+
+        if (node.getTextSelectionStart() != -1 &&
+                node.getTextSelectionStart() != node.getTextSelectionEnd()) {
+            return node.getText().subSequence(
+                        node.getTextSelectionStart(),
+                        node.getTextSelectionEnd())
+                    .toString();
+        }
+
+        for (int i = 0; i < node.getChildCount(); i++) {
+            final String selected = walkViews(node.getChildAt(i), depth + 1);
+            if (selected != null)
+                return selected;
+        }
+
+        return null;
     }
 }
