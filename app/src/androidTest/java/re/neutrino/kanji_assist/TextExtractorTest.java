4e1ffcbc915520c0a54f4b6d54f558de8493ef5b
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2016-12-17 04:26:21 +0100

    Add testExtract_textEdit
    TODO Make longPressHome() work
    
    Add debug-build-only TextExtractTextViewActivity
    TODO Rename TestExtractText{View -> Edit}Activity

--- app/src/androidTest/java/re/neutrino/kanji_assist/TextExtractorTest.java
+++ app/src/androidTest/java/re/neutrino/kanji_assist/TextExtractorTest.java
@@ -1,21 +1,55 @@
 package re.neutrino.kanji_assist;
 
 import android.app.assist.AssistStructure;
+import android.content.Intent;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.lang.reflect.InvocationTargetException;
 
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static re.neutrino.kanji_assist.TextExtractor.getSelectedText;
 
 @RunWith(AndroidJUnit4.class)
-public class TextExtractorTest {
+public class TextExtractorTest extends AssistTest {
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        setThisAssistant();
+    }
+
     @Test
-    public void withEmptyStructure() throws Exception {
+    public void testExtract_emptyStructure() throws Exception {
         final AssistStructure empty = new AssistStructure();
 
         assertThat(getSelectedText(empty), nullValue());
+    }
+
+    @Test
+    public void testExtract_textEdit() throws UiObjectNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, NoSuchFieldException {
+        startActivityAsNewTask(new Intent(context,
+                re.neutrino.kanji_assist.TestExtractTextViewActivity.class));
+
+        final UiObject edit = device.findObject(new UiSelector()
+                .resourceId("re.neutrino.kanji_assist:id/editText"));
+        edit.setText("first second");
+        edit.longClickTopLeft();
+
+        requestAssist();
+    }
+
+    private void requestAssist() throws UiObjectNotFoundException, NoSuchMethodException, NoSuchFieldException, IllegalAccessException, InvocationTargetException {
+        longPressHome();
+    }
+
+    private void longPressHome() throws UiObjectNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, NoSuchFieldException {
+        throw new Error("Not implemented");
     }
 }
---
+++ app/src/debug/java/re/neutrino/kanji-assist/TestExtractTextViewActivity.java
@@ -0,0 +1,13 @@
+package re.neutrino.kanji_assist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestExtractTextViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_extract_text_view);
+    }
+}
