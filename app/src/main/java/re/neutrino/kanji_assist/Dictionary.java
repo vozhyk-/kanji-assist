eeae8eca94a4d05090f8663f9174272905da3719
Aleksander Mistewicz <a.mistewicz@gmail.com>
2016-12-19 21:04:55 +0100

    Add JSON response parsing

--- app/src/main/java/re/neutrino/kanji_assist/Dictionary.java
+++ app/src/main/java/re/neutrino/kanji_assist/Dictionary.java
@@ -1,6 +1,5 @@
 package re.neutrino.kanji_assist;
 
-import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
--- app/src/main/java/re/neutrino/kanji_assist/DictionaryBackgroundTask.java
+++ app/src/main/java/re/neutrino/kanji_assist/DictionaryBackgroundTask.java
@@ -1,23 +1,22 @@
 package re.neutrino.kanji_assist;
 
 import android.os.AsyncTask;
-import android.os.Message;
 import android.util.JsonReader;
 import android.util.Log;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 class DictionaryBackgroundTask extends AsyncTask<String, Void, String> {
     final static String baseApiUrl = "http://jisho.org/api/v1/search/words?keyword=";
+    final static String debugName = "backgroundTask";
     HttpURLConnection connection;
-    BufferedReader reader;
     URL url;
     TextView textView;
 
@@ -27,7 +26,7 @@ class DictionaryBackgroundTask extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String... strings) {
-        Log.d("backgroundTask", "start");
+        Log.d(debugName, "start");
         String text = strings[0];
         try {
             url = new URL(baseApiUrl + text);
@@ -35,18 +34,87 @@ class DictionaryBackgroundTask extends AsyncTask<String, Void, String> {
             connection.connect();
 
             InputStream inputStream = connection.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-            JsonReader jsonReader = new JsonReader(reader);
-            jsonReader.close();
+            ArrayList<Definition> definitionArrayList = readJsonStream(inputStream);
+            Log.d(debugName, definitionArrayList.toString());
             connection.disconnect();
-            return "fetch: success";
+            return definitionArrayList.toString();
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Log.d("backgroundTask", "end");
+        Log.d(debugName, "end");
         return null;
+    }
+
+    public ArrayList<Definition> readJsonStream(InputStream in) throws IOException {
+        JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+        try {
+            return readResponse(reader);
+        } finally {
+            reader.close();
+        }
+    }
+
+    public ArrayList<Definition> readResponse(JsonReader reader) throws IOException {
+        ArrayList<Definition> definitions = new ArrayList<>();
+        String data = null;
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("data")) {
+                reader.beginArray();
+                while (reader.hasNext()) {
+                    reader.beginObject();
+                    while (reader.hasNext()) {
+                        String inside_name = reader.nextName();
+                        if (inside_name.equals("japanese")) {
+                            reader.beginArray();
+                            while (reader.hasNext()) {
+                                definitions.add(readDefinition(reader));
+                            }
+                            reader.endArray();
+                        } else {
+                            reader.skipValue();
+                        }
+                    }
+                    reader.endObject();
+                }
+                reader.endArray();
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        return definitions;
+    }
+
+    class Definition {
+        String word;
+        String reading;
+
+        public String toString() {
+            return "word: " + word + ", reading: " + reading;
+        }
+    }
+
+    Definition readDefinition(JsonReader reader) throws IOException {
+        Definition definition = new Definition();
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("word")) {
+                definition.word = reader.nextString();
+            } else if (name.equals("reading")) {
+                definition.reading = reader.nextString();
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        Log.d(debugName, definition.toString());
+        return definition;
     }
 
     @Override
