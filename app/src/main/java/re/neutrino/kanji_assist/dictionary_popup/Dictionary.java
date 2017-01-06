2c6d7951b9d6bea93d2c9e4437e3359efd068dff
Aleksander Mistewicz <a.mistewicz@gmail.com>
2017-01-06 13:47:54 +0100

    Show all words, readings and definitions

--- app/src/main/java/re/neutrino/kanji_assist/dictionary_popup/Dictionary.java
+++ app/src/main/java/re/neutrino/kanji_assist/dictionary_popup/Dictionary.java
@@ -16,17 +16,16 @@
 
 package re.neutrino.kanji_assist.dictionary_popup;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 class Dictionary {
-    void get_definition(String text, TextView textView) {
-        DictionaryBackgroundTask dictionaryBackgroundTask = new DictionaryBackgroundTask(textView);
+    void get_definition(DictionaryPopup dictionaryPopup, String text) {
+        DictionaryBackgroundTask dictionaryBackgroundTask = new
+                DictionaryBackgroundTask(dictionaryPopup);
         Log.d("dictionary", text);
-        Log.d("dictionary", textView.getText().toString());
         dictionaryBackgroundTask.execute(text);
-        textView.setText("fetch: in progress");
-        textView.setVisibility(View.VISIBLE);
     }
 }
