4a58305610c18ff37c8ef1adccbc108ffd9c4482
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2017-02-11 16:23:49 +0100

    #14 Add test
    Use Espresso instead of UiDevice.

--- app/src/androidTest/java/re/neutrino/kanji_assist/AssistStructureVisualizerTest.java
+++ app/src/androidTest/java/re/neutrino/kanji_assist/AssistStructureVisualizerTest.java
@@ -19,25 +19,43 @@ package re.neutrino.kanji_assist;
 
 import android.content.Intent;
 import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import re.neutrino.kanji_assist.assist_structure.FakeAssistStructure;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class AssistStructureVisualizerTest extends BasicTest {
 
-    @Test
-    public void assistStructureVisualizerTest() throws Exception {
-        final FakeAssistStructure structure = new AssistStructureDebugUtil(context)
-                .readAssistStructure(R.raw.settings_with_baselines);
+    @Rule
+    public ActivityTestRule<AssistStructureVisualizerActivity> rule =
+            new ActivityTestRule<AssistStructureVisualizerActivity>
+                    (AssistStructureVisualizerActivity.class, true, false);
 
-        startActivityAsNewTask(
-                new Intent(context, AssistStructureVisualizerActivity.class)
-                        .putExtra("structure", structure));
+    @Test
+    public void visualizer_edittext_hint() {
+        rule.launchActivity(intentWithFixture("edittext_hint"));
+
+        // when no text is entered, hint is used
+        onView(withText("場所を追加")).check(matches(isDisplayed()));
+
+        // when some text is entered, hint is not used
+        onView(withText("test title")).check(matches(isDisplayed()));
+        onView(withText("タイトルを入力")).check(doesNotExist());
     }
 
+    private Intent intentWithFixture(String fixtureName) {
+        return new Intent(context, AssistStructureVisualizerActivity.class)
+                .putExtra(AssistStructureVisualizerActivity.FIXTURE_NAME_KEY,
+                        fixtureName);
+    }
 }
